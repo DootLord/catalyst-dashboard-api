7751054dd9f37a2df198f804108a7c67e1e6be49
DootLord <shaunoman840@gmail.com>
2024-11-12 23:26:03 +0000

    Simple API for react-webapp. Counter does counting!!!

--- app.ts
+++ app.ts
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get("/test", (req, res) => {
-  res.send("Hello World!");
+let counter = 0;
+app.use(express.json());
+
+app.get("/count", (req, res) => {
+    res.send({
+        value: counter
+    });
+}); 
+
+app.post("/count", (req, res) => {
+    if (!req.body) {
+        return res.status(400).send("Request body is missing");
+    }
+    const { value } = req.body;
+    console.log(value);
+    counter += value;
+
+    res.send("" + counter);
 });
 
 app.listen(port, () => {
